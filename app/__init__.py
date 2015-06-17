5c200dbd6fc76a30e5d6c9041c65c7c864b14fb4
Ramil Nugmanov <stsouko@live.ru>
2015-06-18 00:19:30 +0300

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -30,6 +30,7 @@ def create_app(configfile=None):
     AppConfig(app, configfile)
     Bootstrap(app)
     login_manager.init_app(app)
+    login_manager.login_view = 'login'
     return app
 
 db = NmrDB()
